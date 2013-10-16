cfdbe91f55daffd0a482b25362748ffe7c9d374c
zhao <zhao@zhao-MacB.local>
2013-10-16 10:36:15 +0800

    creat project

---
+++ Maomao/Maomao/MAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MAppDelegate.h
+//  Maomao
+//
+//  Created by  zhao on 13-10-16.
+//  Copyright (c) 2013年 BangQu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Maomao/Maomao/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Maomao
+//
+//  Created by  zhao on 13-10-16.
+//  Copyright (c) 2013年 BangQu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MAppDelegate class]));
+    }
+}
