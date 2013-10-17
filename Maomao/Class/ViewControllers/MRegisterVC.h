b5238e8502105d8ac87c49be0d12532533803b3f
freeman22 <zhao@zhao-macb.mshome.net>
2013-10-17 15:59:44 +0800

    create login and register class

---
+++ Maomao/Class/ViewControllers/MRegisterVC.h
@@ -0,0 +1,13 @@
+//
+//  MRegisterVC.h
+//  Maomao
+//
+//  Created by  zhao on 13-10-17.
+//  Copyright (c) 2013年 BangQu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MRegisterVC : UIViewController
+
+@end
---
+++ Maomao/Class/ViewControllers/MRegisterVC.m
@@ -0,0 +1,38 @@
+//
+//  MRegisterVC.m
+//  Maomao
+//
+//  Created by  zhao on 13-10-17.
+//  Copyright (c) 2013年 BangQu. All rights reserved.
+//
+
+#import "MRegisterVC.h"
+
+@interface MRegisterVC ()
+
+@end
+
+@implementation MRegisterVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
